6038a3b7641ee91f76e1bbd20d8ac1f7e8cbd3c7
Camila <contactcamilasanchez@gmail.com>
2021-11-08 23:58:40 +0000

    Fixes.

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,38 +1,11 @@
-import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt
+from flask import Flask, request, jsonify, url_for, Blueprint
+from api.utils import APIException, generate_sitemap
+from api.models import db, User
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
-CORS(app)
-setup_admin(app)
-app.config["JWT_SECRET_KEY"] = "ha82hbk50gjqva978bru3ifeid20al0l9j2ks8d4kd72dncjafqw093jb8c0zz1"
-jwt = JWTManager(app)
-bcrypt = Bcrypt(app)
+api = Blueprint('api', __name__)
 
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
-
-@app.route('/signup', methods=['POST'])
+@api.route('/signup', methods=['POST'])
 def signup_post():
     body = request.get_json()
 
@@ -56,24 +29,19 @@ def signup_post():
     if user:
         raise APIException('There is already an account with this email.', status_code=400)
 
-    new_user = User(name=body['name'], lastname=body['lastname'], email=body['email'], password=bcrypt.generate_password_hash(body['password']).decode('utf-8'))
+    new_user = User(name=body['name'], lastname=body['lastname'], email=body['email'], password='password')
     db.session.add(new_user)
     db.session.commit()
-
     return "ok", 200
 
-@app.route("/login", methods=["POST"])
+@api.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
     
     user = User.query.filter_by(email = email).first()
-    if not user or not bcrypt.check_password_hash(user.password, password):
+    if not user or user.password != password:
         raise APIException('Please check your login details and try again.', status_code=400)
 
     access_token = create_access_token(identity=user.id)
     return jsonify(access_token=access_token)
-
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
