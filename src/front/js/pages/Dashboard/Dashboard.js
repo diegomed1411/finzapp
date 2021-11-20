d37212d2fcde471898addd88b740e277f4e0cbcb
Camila <contactcamilasanchez@gmail.com>
2021-11-20 15:20:50 +0000

    .

--- src/front/js/pages/Dashboard/Dashboard.js
+++ src/front/js/pages/Dashboard/Dashboard.js
@@ -3,6 +3,6 @@ import React from "react";
 import "./styles.scss";
 import Layout from "../../component/Layout/Layout";
 
-const Dashboard = () => <Layout />;
+const Dashboard = () => <Layout>Here goes the dashboard components</Layout>;
 
 export default Dashboard;
